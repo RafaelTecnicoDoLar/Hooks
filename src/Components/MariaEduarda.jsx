054de09005032ee403f6d3eb1e806fd99117dcb2
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-11-09 12:04:15 -0300

    calculadora +-*/

--- src/Components/MariaEduarda.jsx
+++ src/Components/MariaEduarda.jsx
@@ -1,27 +1,67 @@
+import { useState } from "react";
+
 function MariaEduarda() {
 
-let n1;
-n1 = 44
+    const [numero1, setNumero1] = useState(1)
+    const [numero2, setNumero2] = useState(1)
 
-let n2;
-n2 = 33;
+    const [conta, setConta] = useState(0)
+    const [operador, setOperador] = useState()
 
-let c1;
-c1 = n1 + n2;
+    let n1;
+    n1 = 1;
 
-    function cliqueMouse() {
-        alert("Porque tantos códigos? Se na vida, nada é programado, e o que mais importa, não tem lógica!");
+    let n2;
+    n2 = 1;
+
+    let Operador;
+
+    function botaoNumero1() {
+        setNumero1(n1 + numero1);
+    }
+
+    function botaoNumero2() {
+        setNumero2(n2 + numero2);
+    }
+
+    function botaoAdicao() {
+        setConta(numero1 + numero2);
+        setOperador("+");
+       
+    }
+
+    function botaoSubtracao() {
+        setConta(numero1 - numero2);
+        setOperador("-");
+        
+    }
+
+    function botaoMultiplicacao() {
+        setConta(numero1 * numero2);
+        setOperador("*");
+        
+    }
+
+    function botaoDivisao() {
+        setConta(numero1 / numero2);
+        setOperador("/");
 
     }
 
     return (
         <>
-        <p>
-            O resultado de {n1} + {n2} = {c1}
+            <p>
+                O resultado de {numero1} {operador} {numero2} = {conta}
 
-        </p>
-        <button onClick={cliqueMouse}>clique aqui</button>
-            
+            </p>
+            <button className="Botao" onClick={botaoNumero1}>   N1 + 1 </button>
+            <button className="Botao" onClick={botaoNumero2}>   N2 + 2 </button>
+
+            <button className="Botao" onClick={botaoAdicao}>        +   </button>
+            <button className="Botao" onClick={botaoSubtracao}>     -   </button>
+            <button className="Botao" onClick={botaoMultiplicacao}> *   </button>
+            <button className="Botao" onClick={botaoDivisao}>       /   </button>
+
         </>
     )
 }
